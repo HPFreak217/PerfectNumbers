8c49228d69a7c9a92d1f9115c40b198cfc6a2ba8
HPFreak217 <carlos.hurtado19@yahoo.com>
2017-05-21 21:40:45 -0700

    Java program that calculates the first 4 numbers.

--- Perfect Numbers/main.cpp
+++ Perfect Numbers/main.cpp
@@ -7,9 +7,115 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <string>
+#include <vector>
+using namespace std;
+void firstFour();
+void perfectNumber(int num);
+int main(int argc, const char * argv[])
+{
+    bool running = true;
+    while (running)
+    {
+        int choice = 0;
+        cout<<"1) Display first four perfect numbers"<<endl;
+        cout<<"2) Perfect number checker"<<endl;
+        cout<<"3) Quit"<<endl;
+        cin>>choice;
+        if(choice == 1)
+        {
+            firstFour();
+        }
+        else if(choice == 2)
+        {
+            bool valid = false;
+            while(!valid)
+            {
+                int num = 0;
+                cout<<"Enter a positive integer or 1 to exit: ";
+                cin>>num;
+                if(num>1)
+                {
+                    perfectNumber(num);
+                    valid = true;
+                }
+                else if(num <1)
+                {
+                    cout<<"-Invalid Number-"<<endl;
+                }
+                else
+                {
+                    valid = true;
+                }
+            }
+        }
+        else if(choice == 3)
+        {
+            cout<<"Exitting...done"<<endl;
+            running = false;
+        }
+    }
     return 0;
+}
+
+void firstFour()
+{
+    vector<int> perfect;
+    vector<int> divisors;
+    bool notPerfect = true;
+    int test = 2;
+    for(int i = 0; i < 4; i++)
+    {
+        int divisible = 1;
+        while(notPerfect)
+        {
+            if(divisible < test)
+            {
+                if(test%divisible == 0)
+                {
+                    divisors.push_back(divisible);
+                }
+                divisible++;
+            }
+            else
+            {
+                int sum = 0;
+                for(int n: divisors)
+                {
+                    sum+=n;
+                }
+                if(sum == test)
+                {
+                    perfect.push_back(test);
+                    divisors.clear();
+                    notPerfect = false;
+                }
+                else
+                {
+                    test++;
+                    divisible = 1;
+                    divisors.clear();
+                }
+            }
+            
+        }
+        test++;
+        notPerfect =  true;
+    }
+    for(int i: perfect)
+    {
+        cout<<i<<endl;
+    }
+}
+
+void perfectNumber(int num)
+{
+    if(num == 6 || num == 28 || num == 496 || num == 8128 || num == 33550336)
+    {
+        cout<<num<<" is a perfect number"<<endl;
+    }
+    else
+    {
+        cout<<num<<" is not a perfect number"<<endl;
+    }
 }
